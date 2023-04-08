c903aa5714455dab1a2aa25c77f21d5b003f3a5d
maerka <72936601+maerka@users.noreply.github.com>
2023-04-08 15:01:12 +0200

    basisWebsite

---
+++ script/script.js
@@ -0,0 +1,33 @@
+// https://brad-portfolio.netlify.app/#
+
+// https://www.freecodecamp.org/news/how-to-create-a-portfolio-website-using-html-css-javascript-and-bootstrap/#how-to-build-the-portfolio-section
+
+// https://getbootstrap.com/docs/5.0/getting-started/introduction/
+
+// https://www.embed-map.com/
+
+
+
+// // add class navbarDark on navbar scroll
+// const header = document.querySelector('#navbar');
+// console.log('HERE',header)
+// window.onscroll = function() {
+//     console.log('scrolling', header)
+//     const top = window.scrollY;
+//     if(top >=100) {
+//         header.classList.add('navbarDark');
+//         console.log('> 100 add')
+//     }
+//     else {
+//         header.classList.remove('navbarDark');
+//         console.log('< 100 remove')
+//     }
+// }
+
+// // collapse navbar after click on small devices
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarSupportedContent')
+
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+// })
